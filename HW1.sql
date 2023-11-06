4f69d14e9bb6d3e4fb2aac68322ce31fc6c3b598
MohammadaliYaghoubi <149804910+MohammadaliYaghoubi@users.noreply.github.com>
2023-11-06 15:05:57 +0330

    HW1.sql

--- HW1.sql
+++ HW1.sql
@@ -72,7 +72,7 @@ INSERT INTO Tables (capacity)
 VALUES
 (2),(4),(4),(6)
 ;
-INSERT INTO Orders (customer_id,table_id)
+INSERT INTO Orders (customer_id,table_id,order_time)
 VALUES
-(2,4),(1,2)
+(2,4,'2020-02-04 10:30:00'),(1,2,'2020-03-04 16:29:00')
 ;
